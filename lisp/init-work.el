7d6cd684542b52e6c6ed219e7f4cacfca0d13b65
gcclll <gccll.love@gmail.com>
2020-08-21 10:10:19 +0800

    update

---
+++ lisp/init-work.el
@@ -0,0 +1,15 @@
+;;; INIT-WORK --- for working
+;;
+;; Author: ZhiCheng Lee <gccll.love@gmail.com>
+;; Copyright © 2020, ZhiCheng Lee, all rights reserved.
+;; Created: 21 August 2020
+;;
+;;; Commentary:
+;;
+;;  
+;;
+;;; Code:
+
+
+(provide 'init-work)
+;;; init-work.el ends here
