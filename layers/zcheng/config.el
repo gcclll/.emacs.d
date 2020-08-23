239fa3bed3430cdbfd2c704d2373371c47bbbd2d
gcclll <gccll.love@gmail.com>
2020-08-23 08:22:39 +0800

    init

---
+++ layers/zcheng/config.el
@@ -0,0 +1,19 @@
+                                        ; -*- lexical-binding: t -*-
+(defvar org-agenda-dir ""
+  "gtd org files location")
+
+(defvar deft-dir ""
+  "deft org files locaiton")
+
+(defvar blog-admin-dir ""
+  "blog-admin files location")
+
+(if (spacemacs/system-is-mswindows)
+    (setq
+     org-agenda-dir "d:/.blog/zcheng.top/orgs"
+     deft-dir "d:/.blog/zcheng.top/orgs"
+     blog-admin-dir "d:/.blog/zcheng.top")
+  (setq
+   org-agenda-dir "~/.blog/zcheng.top/orgs"
+   deft-dir "~/.blog/zcheng.top/orgs"
+   blog-admin-dir "~/.blog/zcheng.top"))
---
+++ layers/zcheng/keybindings.el
@@ -0,0 +1,38 @@
+;;; keybindings.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Zhicheng Lee
+;; Maintainer: Zhicheng Lee
+;; Email: gccll.love@gmail.com
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: https://www.zcheng.top
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(spacemacs/set-leader-keys "yi" 'yas/insert-snippet)
+
+(provide 'keybindings)
+
+;;; keybindings.el ends here
