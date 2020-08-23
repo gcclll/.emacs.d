1ee732260e77405af41a8c63bb1327e8da5ee967
gcclll <gccll.love@gmail.com>
2020-08-23 15:59:40 +0800

    2020-08-23 15:59:40

--- layers/zcheng/funcs.el
+++ layers/zcheng/funcs.el
@@ -197,8 +197,7 @@ With PREFIX, cd to project root."
 (defun gcl/git-push-delete-window ()
 	(progn
 		(message "Git push should done or running in background.")
-		(when (string-match-p "\\`\\*interpretation\\*\\'" (buffer-name))
-      (kill-buffer buffer-name))))
+		(delete-window)))
 
 (provide 'funcs)
 
