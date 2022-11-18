25fe7dc70bc9f7459cc4fbbe95e395bbf098f6ea
gcclll <gccll.love@gmail.com>
2022-11-18 23:04:08 +0800

    2022-11-18 23:04:08

--- init.el
+++ init.el
@@ -1,28 +1,5 @@
-(require 'package)
 (setq package-archives '(("gnu"   . "http://1.15.88.122/gnu/")
 			 ("melpa" . "http://1.15.88.122/melpa/")))
-
-(package-initialize)
-
-(assq-delete-all 'org package--builtins)
-(assq-delete-all 'org package--builtin-versions)
-
-;; Setup `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-
-;; Should set before loading `use-package'
-(eval-and-compile
-  (setq use-package-always-ensure t)
-  (setq use-package-always-defer t)
-  (setq use-package-expand-minimally t)
-  (setq use-package-enable-imenu-support t))
-
-
-(eval-when-compile
-  (require 'use-package))
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
