ff2fa4134fe708601aab734f75adf1c36d996243
gcclll <gccll.love@gmail.com>
2022-11-19 16:16:03 +0800

    2022-11-19 16:16:03

--- init.el
+++ init.el
@@ -398,57 +398,93 @@ one, an error is signaled."
     :config
     (vertico-mode))
 
-  (use-package which-key
-    :hook (after-init . which-key-mode)
-    :ensure t
-    :init
-    (setq which-key-side-window-location 'bottom)
-    (setq which-key-show-early-on-C-h t)
-    (setq which-key-idle-delay 0.1)
-    ;;(setq which-key-idle-secondary-delay 0.05)
-    (which-key-mode)
+(use-package which-key
+  :hook (after-init . which-key-mode)
+  :ensure t
+  :init
+  (setq which-key-side-window-location 'bottom)
+  (setq which-key-show-early-on-C-h t)
+  (setq which-key-idle-delay 0.1)
+  ;;(setq which-key-idle-secondary-delay 0.05)
+  (which-key-mode)
+  )
+
+(use-package evil
+  :ensure t
+  :init
+  (evil-mode)
+  :config
+  ;; 退出编辑模式后光标留在原地
+  (setq evil-move-cursor-back nil)
+  ;; 让回车，TAB，空格键保持原来的功能
+  (with-eval-after-load 'evil-maps
+    (define-key evil-motion-state-map (kbd "RET") nil)
+    (define-key evil-motion-state-map (kbd "TAB") nil)
+    (define-key evil-motion-state-map (kbd "SPC") nil))
+  (progn
+    (setcdr evil-insert-state-map nil)
+    (define-key evil-insert-state-map [escape] 'evil-normal-state)
+
+    ;; --- 解绑一些按键
+    (evil-global-set-key 'normal (kbd "c") nil)
+
+    (setcdr evil-insert-state-map nil)
+    (define-key evil-insert-state-map [escape] 'evil-normal-state)
+    (setq-default evil-ex-search-persistent-highlight nil)
+
+    (define-key evil-motion-state-map (kbd "0") 'evil-end-of-line)
+
+    (evil-global-set-key 'normal "f" 'evil-avy-goto-char)
+    (evil-global-set-key 'normal "w" 'evil-avy-goto-word-1)
+    (evil-global-set-key 'motion "-" 'org-decrease-number-at-point)
+    (evil-global-set-key 'motion "+" 'org-increase-number-at-point)
+
+    (evil-global-set-key 'normal (kbd "gd") 'xref-find-definitions)
+    (evil-global-set-key 'normal (kbd "gb") 'xref-pop-marker-stack)
+    (evil-global-set-key 'normal (kbd "gc") 'show-commit-and-preserve-window)
+
+    (evil-global-set-key 'normal (kbd "cc") 'evilnc-copy-and-comment-lines)
     )
+  )
 
-  (use-package evil
-    :ensure t
-    :init
-    (evil-mode)
-    :config
-    ;; 退出编辑模式后光标留在原地
-    (setq evil-move-cursor-back nil)
-    ;; 让回车，TAB，空格键保持原来的功能
-    (with-eval-after-load 'evil-maps
-      (define-key evil-motion-state-map (kbd "RET") nil)
-      (define-key evil-motion-state-map (kbd "TAB") nil)
-      (define-key evil-motion-state-map (kbd "SPC") nil))
-    (progn
-      (setcdr evil-insert-state-map nil)
-      (define-key evil-insert-state-map [escape] 'evil-normal-state)
+(use-package evil-nerd-commenter
+  :ensure t)
 
-      ;; --- 解绑一些按键
-      (evil-global-set-key 'normal (kbd "c") nil)
+(use-package evil-surround
+  :ensure t
+  :config
+  (setq-default evil-surround-pairs-alist
+		            '((?\( . ("(" . ")"))
+                  (?\[ . ("[" . "]"))
+                  (?\{ . ("{" . "}"))
 
-      (setcdr evil-insert-state-map nil)
-      (define-key evil-insert-state-map [escape] 'evil-normal-state)
-      (setq-default evil-ex-search-persistent-highlight nil)
+                  (?\) . ("( " . " )"))
+                  (?\] . ("[ " . " ]"))
+                  (?\} . ("{ " . " }"))
 
-      (define-key evil-motion-state-map (kbd "0") 'evil-end-of-line)
+                  (?# . ("#{" . "}"))
+                  (?b . ("(" . ")"))
+                  (?B . ("{" . "}"))
+                  (?> . ("<" . ">"))
 
-      (evil-global-set-key 'normal "f" 'evil-avy-goto-char)
-      (evil-global-set-key 'normal "w" 'evil-avy-goto-word-1)
-      (evil-global-set-key 'motion "-" 'org-decrease-number-at-point)
-      (evil-global-set-key 'motion "+" 'org-increase-number-at-point)
+                  (?\/ . ("/* " . " */"))
 
-      (evil-global-set-key 'normal (kbd "gd") 'xref-find-definitions)
-      (evil-global-set-key 'normal (kbd "gb") 'xref-pop-marker-stack)
-      (evil-global-set-key 'normal (kbd "gc") 'show-commit-and-preserve-window)
+                  ;; Single-quoted strings
+                  (?\' . ("'" . "'"))
 
-      (evil-global-set-key 'normal (kbd "cc") 'evilnc-copy-and-comment-lines)
-      )
-    )
+                  ;; Emacs-style quotes
+                  (?\` . ("`" . "'"))
+		              ;; javascript
+                  (?\` . ("`" . "`"))
 
-  (use-package evil-nerd-commenter
-    :ensure t)
+                  ;; Python multi-line strings
+                  (?d . ("\"\"\"" . "\"\"\""))
+                  (?D . ("'''" . "'''"))
+
+                  (?t . evil-surround-read-tag)
+                  (?< . evil-surround-read-tag)
+                  (?f . evil-surround-function)))
+  (global-evil-surround-mode 1))
 
 ;; (evil-set-initial-state 'color-rg-mode 'emacs)
 ;; (evil-set-initial-state 'multi-vterm-mode 'emacs)
@@ -512,7 +548,9 @@ one, an error is signaled."
 
   (+general-global-menu! "apps" "a"
 			 "a" 'org-agenda
-			 "c" 'agenda)
+			 "c" 'agenda
+       ;; "l" 'app-launcher-run-app
+       )
 
   (+general-global-menu! "buffer" "b"
 			 "b" 'consult-buffer
@@ -654,7 +692,7 @@ one, an error is signaled."
    "M-#" 'consult-register-load
    "M-;" 'evilnc-comment-or-uncomment-lines
    ;; "M-e" 'emojify-insert-emoji
-   ;; "M-d" 'dash-at-point
+   "M-d" 'consult-dash
    ;; "M-j" 'rime-inline-ascii
    "M-i" 'consult-imenu
    ;; "M-m" 'blamer-show-posframe-commit-info
@@ -1954,6 +1992,11 @@ _k_: down      _a_: combine       _q_: quit
   :bind
   (("C-c g f" . #'consult-ls-git)
    ("C-c g F" . #'consult-ls-git-other-window)))
+
+(use-package consult-dash
+  :config
+  ;; Use the symbol at point as initial search term
+  (consult-customize consult-dash :initial (thing-at-point 'symbol)))
 
 (use-package wgrep
   :config
